9e1d1ec1e94f3bd589f5a905db783d6c4fc49022
Lenovo-PC <e999or@bk.ru>
2020-03-15 00:24:04 +0300

    first commit

---
+++ src/test/java/com/github/e999or/lesson18/SimpleTest.java
@@ -0,0 +1,85 @@
+package com.github.e999or.lesson18;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleTest.class);
+    private WebDriver webDriver;
+
+    @BeforeMethod
+    public void beforeTestMethod() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        LOG.info("Before ComplexTest method");
+    }
+
+
+    @Test
+    public void shouldAnswerWithTrue() throws InterruptedException {
+        webDriver.get("https://savkk.github.io/selenium-practice/");
+
+        webDriver.findElement(By.id("alerts")).click();
+        webDriver.findElement(By.className("get")).click();
+        Alert alert = webDriver.switchTo().alert();
+        String [] getPassword = alert.getText().split(":");
+        String noSpacePassword = getPassword[1].replaceAll(" ", "");
+        alert.accept();
+        webDriver.findElement(By.className("set")).click();
+        alert.sendKeys(noSpacePassword);
+        alert.accept();
+        WebElement great = webDriver.findElement(By.xpath("//label"));
+        Assert.assertEquals(great.getAttribute("innerText"),"Great!");
+        webDriver.findElement(By.className("return")).click();
+        alert.accept();
+
+        webDriver.findElement(By.id("alerts")).click();
+        webDriver.findElement(By.className("get")).click();
+        alert.accept();
+        webDriver.findElement(By.className("set")).click();
+        alert.sendKeys("&&&&&");
+        try{
+            webDriver.findElement(By.xpath("//label"));
+        }catch (UnhandledAlertException e){
+            webDriver.findElement(By.className("get")).click();
+            Alert alertNegativ = webDriver.switchTo().alert();
+            String [] getPasswordNegativ = alertNegativ.getText().split(":");
+            String noSpacePasswordNegativ = getPasswordNegativ[1].replaceAll(" ", "");
+            alertNegativ.accept();
+            webDriver.findElement(By.className("set")).click();
+            alertNegativ.sendKeys(noSpacePasswordNegativ);
+            alertNegativ.accept();
+            webDriver.findElement(By.className("return")).click();
+            alertNegativ.accept();
+        }
+
+        webDriver.findElement(By.id("table")).click();
+        webDriver.findElement(By.id("customers")).click();
+        webDriver.findElement(By.xpath("//td[text()='Island Trading']/parent:: node()/td/input")).click();
+        webDriver.findElement(By.xpath("//input[@value='Delete']")).click();
+        webDriver.findElement(By.xpath("//label[text()='Company']/following::input")).sendKeys("Айтеко");
+        webDriver.findElement(By.xpath("//label[text()='Contact']/following::input")).sendKeys("9037777");
+        webDriver.findElement(By.xpath("//label[text()='Country']/following::input")).sendKeys("Россия");
+        webDriver.findElement(By.xpath("//input[@value='Add']")).click();
+
+        WebElement returnToMenuTable = webDriver.findElement(By.linkText("Great! Return to menu"));
+        Assert.assertEquals(returnToMenuTable.getAttribute("innerText"),"Great! Return to menu");
+        returnToMenuTable.click();
+
+
+
+
+        Thread.sleep(10000);
+        webDriver.quit();
+
+    }
+
+
+
+}
